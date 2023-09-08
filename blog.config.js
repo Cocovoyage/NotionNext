e5df451adca348ab69583d631bdf56ab5f8ce735
Cocovoyage <140452636+Cocovoyage@users.noreply.github.com>
2023-09-08 19:38:20 -0400

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,13 +13,13 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Salut，我是一个热爱大自然的人, Hi，我是一个喜欢到处逛的人,Hola，我是一个安于平淡生活的人,欢迎来到我的博客🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Salut，我是一个热爱大自然的人, Hi，我是一个喜欢到处逛的人,Hola，我是一个安于平淡生活的人,欢迎来到我的博客👒',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Coco 休闲游-北美', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Coco 休闲游的个人博客', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-1y3jbhfi6-cocovoyage.vercel.app/', // 网站地址
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '喜爱休闲游的Coco', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Coco休闲游的个人博客', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-1y3jbhfi6-cocovoyage.vercel.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
